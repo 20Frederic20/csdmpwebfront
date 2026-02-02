837ccc1d9e6150df9cdd9a3486a40c441b620c54
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-02-02 17:00:01 +0100

    update dashboard layout with sidebar

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+//   const user = await getCurrentUser();
+
+//   if (!user) {
+//     redirect('/login');
+//   }
+
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
