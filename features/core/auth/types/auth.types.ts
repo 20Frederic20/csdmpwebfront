0e3b6296d7b719c078bdf44fe39a0cfc41ae5d38
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-02-02 17:02:27 +0100

    default auth types

---
+++ features/core/auth/types/auth.types.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id: string;
+  email: string;
+  name?: string;
+  role?: 'user' | 'admin';
+}
+
+export interface TokenPayload extends User {
+  exp: number;
+  iat: number;
+  sub: string;
+}
+
+export interface AuthResponse {
+  access_token: string;
+  expires_in: number;
+  refresh_token?: string;
+  refresh_expires_in?: number;
+}
