7370c9353960a9fb7e51b9db31b6aa8f88983a54
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-02-17 00:26:28 +0100

    appoint components index

---
+++ features/appointments/components/index.ts
@@ -0,0 +1,3 @@
+export { ViewAppointmentModal } from './view-appointment-modal';
+export { EditAppointmentModal } from './edit-appointment-modal';
+export { CreateAppointmentModal } from './create-appointment-modal';
